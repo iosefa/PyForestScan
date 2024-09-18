1a2f4fce43c964c239cf42e068bb6b610d315044
Iosefa Percival <ipercival@gmail.com>
2024-09-18 12:44:53 -1000

    Update setup.py
    minor version update for python 3.12

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyforestscan",
-    version="0.1.0",
+    version="0.1.1",
     author="Joseph Emile Honour Percival",
     author_email="ipercival@gmail.com",
     description="UAV image analysis for forest ecology",
