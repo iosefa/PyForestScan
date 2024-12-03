f8ef6bf06cb34c6d56df1b452416fa3e457c0d7e
iosefa <ipercival@gmail.com>
2024-12-02 21:15:42 -1000

    Bump version to 0.2.1 in setup.py.
    This update reflects minor changes or improvements to the package. Ensure dependencies and configurations are compatible with this version update.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyforestscan",
-    version="0.2.0",
+    version="0.2.1",
     author="Joseph Emile Honour Percival",
     author_email="ipercival@gmail.com",
     description="Analyzing forest structure using aerial LiDAR data",
